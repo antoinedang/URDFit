2bbdec784d1c79775e96224e7d748a6e7b44ed4d
Antoine Dangeard <antoine.dangeard@gmail.com>
2025-02-12 15:49:33 -0500

    able to calculate gradients now!

--- urdfit/__init__.py
+++ urdfit/__init__.py
@@ -1,12 +1,3 @@
-import os
-from gymnasium.envs.registration import register
-
-# Get the directory containing the __init__.py file
-init_file_path = os.path.abspath(__file__)
-SIM_XML_PATH = os.path.dirname(init_file_path) + "/assets/world.xml"
-GREEN_SCREEN_SIM_XML_PATH = (
-    os.path.dirname(init_file_path) + "/assets/green_screen_world.xml"
-)
-
-# Register environments
-register(id="CPUHumanoid", entry_point="simulation.cpu_env:CPUEnv")
+class URDFit:
+    def __init__(self, urdf):
+        self.urdf = urdf
