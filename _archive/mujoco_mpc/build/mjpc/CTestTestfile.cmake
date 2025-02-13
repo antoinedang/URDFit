9552340114c1e8c5da8439596497a9ea73cdd0bc
Antoine Dangeard <antoine.dangeard@gmail.com>
2025-02-13 15:32:22 -0500

    start setting up mujoco mpc

--- _archive/mujoco_mpc/build/mjpc/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Humanoid-MPC/mujoco_mpc/mjpc
-# Build directory: /Humanoid-MPC/mujoco_mpc/build/mjpc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tasks")
-subdirs("test")
-subdirs("grpc")
